68c70cd5312fb5d5780ddcee8e8b8f7dd43723c7
shani gupta <shanipratap6615@gmail.com>
2020-12-17 13:54:00 +0530

    map complete
    map mai location lag gaya hai, aur map complete ho gaya hai

--- app/src/main/java/com/truck/transfly/Activty/MapActivity.java
+++ app/src/main/java/com/truck/transfly/Activty/MapActivity.java
@@ -1,20 +1,41 @@
 package com.truck.transfly.Activty;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.core.content.ContextCompat;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -24,21 +45,36 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCanceledListener;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.navigation.NavigationView;
 import com.google.maps.android.ui.IconGenerator;
+import com.nabinbhandari.android.permissions.PermissionHandler;
+import com.nabinbhandari.android.permissions.Permissions;
 import com.truck.transfly.R;
+
+import java.util.ArrayList;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mGoogleMap;
-    private double LatituteOfTajMahal=27.173891;
-    private double LongitudeOfTajMahal=78.042068;
+    private double LatituteOfTajMahal = 27.173891;
+    private double LongitudeOfTajMahal = 78.042068;
 
     private double new1 = 27.209745239842444, new2 = 78.32392058136335;
     private double new3 = 27.56650835174797, new4 = 78.65772736253567;
     private double new5 = 27.52156896657124, new6 = 78.57223839225884;
+    private double tulsipurLat=27.5786526, tulsipurLong=82.4505776;
     private DrawerLayout drawerLayout;
     private CardView viewById;
+    private SettingsClient mSettingsClient;
+    private LocationSettingsRequest mLocationSettingsRequest;
+    private FusedLocationProviderClient mLocationClient;
+    private static final int REQUEST_CHECK_SETTINGS = 214;
+    private static final int REQUEST_ENABLE_GPS = 516;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +91,11 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         mapFragment.getMapAsync(this::onMapReady);
 
+        mLocationClient = LocationServices.getFusedLocationProviderClient(MapActivity.this);
+
         drawerLayout = findViewById(R.id.drawer_layout);
 
-         viewById = findViewById(R.id.drawer_icon);
+        viewById = findViewById(R.id.drawer_icon);
 
         viewById.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -75,34 +113,273 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             }
         });
 
+        findViewById(R.id.my_location).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                LocationManager manager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+                if (permissionIsGranted() && manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+                    if (ActivityCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(MapActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+
+                    mLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Location> task) {
+
+                            Location result = task.getResult();
+
+                            if (task.isSuccessful() && result!=null) {
+
+                                goToLocationWithAnimation(result.getLatitude(), result.getLongitude());
+
+                            } else {
+
+                                Toast.makeText(MapActivity.this, "Click again On Location Button! GPS setup is not Ready", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                        }
+                    });
+
+                } else {
+
+                    getCurrentLocation();
+
+                }
+
+            }
+        });
+
+        getCurrentLocation();
+
+    }
+
+    private void goToLocationWithAnimation(double latitude, double longitude) {
+
+        LatLng latLng = new LatLng(latitude, longitude);
+
+        CameraUpdate cameraUpdateFactory = CameraUpdateFactory.newLatLngZoom(latLng, 9);
+
+        mGoogleMap.animateCamera(cameraUpdateFactory, 500, new GoogleMap.CancelableCallback() {
+            @Override
+            public void onFinish() {
+
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            switch (resultCode) {
+                case Activity.RESULT_OK:
+                    doLocationWork();
+                    break;
+                case Activity.RESULT_CANCELED:
+                    Log.e("GPS", "User denied to access location");
+                    openGpsEnableSetting();
+                    break;
+            }
+        } else if (requestCode == REQUEST_ENABLE_GPS) {
+            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+            boolean isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            if (!isGpsEnabled) {
+                openGpsEnableSetting();
+            } else {
+
+                doLocationWork();
+            }
+        }
+    }
+
+    private void openGpsEnableSetting() {
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivityForResult(intent, REQUEST_ENABLE_GPS);
+    }
+
+    private void getCurrentLocation() {
+
+        if (permissionIsGranted()) {
+
+            openDialogOfSettingGps();
+
+        } else {
+
+            String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
+            String rationale = "Please provide location permission so that you can ...";
+            Permissions.Options options = new Permissions.Options()
+                    .setRationaleDialogTitle("Info")
+                    .setSettingsDialogTitle("Warning");
+
+            Permissions.check(this/*context*/, permissions, rationale, options, new PermissionHandler() {
+                @Override
+                public void onGranted() {
+
+                    openDialogOfSettingGps();
+
+                }
+
+                @Override
+                public void onDenied(Context context, ArrayList<String> deniedPermissions) {
+                    // permission denied, block the feature.
+                }
+            });
+
+        }
+
+    }
+
+    private void openDialogOfSettingGps() {
+
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(new LocationRequest().setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY));
+        builder.setAlwaysShow(true);
+        mLocationSettingsRequest = builder.build();
+
+        mSettingsClient = LocationServices.getSettingsClient(MapActivity.this);
+
+        mSettingsClient
+                .checkLocationSettings(mLocationSettingsRequest)
+                .addOnSuccessListener(new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+
+                        doLocationWork();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        int statusCode = ((ApiException) e).getStatusCode();
+                        switch (statusCode) {
+                            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                                try {
+                                    ResolvableApiException rae = (ResolvableApiException) e;
+                                    rae.startResolutionForResult(MapActivity.this, REQUEST_CHECK_SETTINGS);
+                                } catch (IntentSender.SendIntentException sie) {
+                                    Log.e("GPS", "Unable to execute request.");
+                                }
+                                break;
+                            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                                Log.e("GPS", "Location settings are inadequate, and cannot be fixed here. Fix in Settings.");
+                        }
+                    }
+                })
+                .addOnCanceledListener(new OnCanceledListener() {
+                    @Override
+                    public void onCanceled() {
+                        Log.e("GPS", "checkLocationSettings -> onCanceled");
+                    }
+                });
+
+    }
+
+    public boolean permissionIsGranted() {
+
+        int permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void doLocationWork() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        mGoogleMap.setMyLocationEnabled(true);
+        mGoogleMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        new Handler(getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                mLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+
+                        if (task.isSuccessful()) {
+
+
+                            Location location = task.getResult();
+
+                            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                            boolean isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+                            if (isGpsEnabled && location!=null) {
+
+                                getoLocation(location.getLatitude(), location.getLongitude());
+
+                            } else {
+
+                                Toast.makeText(MapActivity.this, "Click On Location Button! GPS setup is not Ready", Toast.LENGTH_SHORT).show();
+
+                            }
+                        }
+
+                    }
+                });
+
+            }
+        },1000);
+
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        mGoogleMap=googleMap;
+        mGoogleMap = googleMap;
 
-        getoLocation(LatituteOfTajMahal,LongitudeOfTajMahal);
+        getoLocation(LatituteOfTajMahal, LongitudeOfTajMahal);
 
         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
         mGoogleMap.getUiSettings().setMapToolbarEnabled(false);
 
-        showMarker(LatituteOfTajMahal,LongitudeOfTajMahal,1,"Allahabad");
-        showMarker(new1,new2,2,"Faizabad");
-        showMarker(new3,new4,3,"Raibraily");
-        showMarker(new5,new6,4,"Nanital");
+        showMarker(LatituteOfTajMahal, LongitudeOfTajMahal, 1, "Allahabad");
+        showMarker(new1, new2, 2, "Faizabad");
+        showMarker(new3, new4, 3, "Raibraily");
+        showMarker(new5, new6, 4, "Nanital");
+        showMarker(tulsipurLat, tulsipurLong, 5, "Tulsipur Mines\nPricing 500 Rupees");
 
         mGoogleMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
             @Override
             public boolean onMarkerClick(Marker marker) {
 
-                if(marker.getTag()==null)
+                if (marker.getTag() == null)
                     return false;
 
-                int position = (int)(marker.getTag());
+                int position = (int) (marker.getTag());
 
-                Toast.makeText(MapActivity.this, ""+position, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MapActivity.this, "" + position, Toast.LENGTH_SHORT).show();
 
                 return false;
             }
@@ -112,15 +389,16 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     private void showMarker(double latituteOfTajMahal, double longitudeOfTajMahal, int postion, String locationAddress) {
 
-        LatLng latLng=new LatLng(latituteOfTajMahal,longitudeOfTajMahal);
+        LatLng latLng = new LatLng(latituteOfTajMahal, longitudeOfTajMahal);
 
         TextView text = new TextView(MapActivity.this);
         text.setTypeface(Typeface.DEFAULT_BOLD);
+        text.setGravity(Gravity.CENTER);
 
         IconGenerator iconFactory = new IconGenerator(this);
         iconFactory.setColor(Color.parseColor("#ff0000"));
         iconFactory.setStyle(IconGenerator.STYLE_RED);
-        MarkerOptions markerOptions=new MarkerOptions();
+        MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(latLng);
         markerOptions.title(locationAddress);
         markerOptions.snippet("This is my spot!");
@@ -134,9 +412,9 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     private void getoLocation(double latituteOfTajMahal, double longitudeOfTajMahal) {
 
-        LatLng latLng=new LatLng(latituteOfTajMahal,longitudeOfTajMahal);
+        LatLng latLng = new LatLng(latituteOfTajMahal, longitudeOfTajMahal);
 
-        CameraUpdate cameraUpdateFactory=CameraUpdateFactory.newLatLngZoom(latLng,9);
+        CameraUpdate cameraUpdateFactory = CameraUpdateFactory.newLatLngZoom(latLng, 9);
 
         mGoogleMap.moveCamera(cameraUpdateFactory);
 
