bc1f20b7017f4684e007983b36c3f3178c18c98a
i_feel_code <32245119+minalarora@users.noreply.github.com>
2020-12-21 09:42:08 +0530

    LOGIN

---
+++ app/src/main/java/com/truck/transfly/Activty/ProfileScreen.java
@@ -0,0 +1,16 @@
+package com.truck.transfly.Activty;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.truck.transfly.R;
+
+public class ProfileScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_screen);
+    }
+}
